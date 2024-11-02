2c42476415992e15de4fab1ee3e64508e67e0117
Maykel Felipe Nekel <maykelnekel@gmail.com>
2024-11-01 22:59:37 -0300

    feat: add max width container to video container

--- src/components/molecules/video-container.tsx
+++ src/components/molecules/video-container.tsx
@@ -28,53 +28,57 @@ export const VideoContainer = () => {
   }, []);
 
   return (
-    <div className="bg-deep_blue flex gap-6 flex-col lg:flex-row-reverse xl:flex-row-reverse items-center justify-between w-full">
-      <div className="relative flex items-center justify-center bg-right bg-no-repeat lg:max-w-[1074px] w-full">
-        <Image
-          src="/video-cover.svg"
-          alt="Button Icon"
-          width={0}
-          height={0}
-          className="flex items-center justify-center bg-right bg-no-repeat w-full lg:max-w-[1200px]"
-        />
-
-        <button
-          onClick={() => setShowVideo(true)}
-          className="absolute flex items-center justify-center flex-col gap-4 "
-        >
+    <div className="bg-deep_blue w-full flex justify-center">
+      <div className="relative max-w-[1800px] flex justify-center w-full flex gap-6 flex-col lg:flex-row-reverse xl:flex-row-reverse items-center justify-between ">
+        <div className="relative flex items-center justify-center bg-right bg-no-repeat lg:max-w-[1074px]">
           <Image
-            className="button-image"
-            src="/playbutton.svg"
+            src="/video-cover.svg"
             alt="Button Icon"
-            width={buttonSize}
-            height={buttonSize}
+            width={0}
+            height={0}
+            className="flex items-center justify-center bg-right bg-no-repeat w-[100%] lg:max-w-[1200px]"
           />
-          <p className="text-sm sm:text-xl">{translations("play-sub-text")}</p>
-        </button>
-      </div>
 
-      <div className="md:absolute pb-4 flex flex-grow flex-col gap-2 justify-start lg:left-[140px] xl:left-[240px] left-[100px] bottom-[100px] lg:bottom-[150px] ">
-        <h1 className="text-3xl sm:text-5xl font-plus-jakarta-sans text-white font-normal drop-shadow-2xl">
-          {translations("name")}
-        </h1>
-        <p className="text-white max-w-[60%] text-base">
-          {translations("role")}
-        </p>
-      </div>
-      {showVideo && (
-        <div
-          onClick={() => setShowVideo(false)}
-          className="fixed  flex flex-col items-center justify-center inset-0 z-10 bg-black bg-opacity-80 w-[100vw] h-[100vh]"
-        >
-          <iframe
-            title="vimeo-player"
-            src="https://player.vimeo.com/video/749148790?h=5fb7c065f9"
-            width="100%"
-            height="56.25%"
-            allowFullScreen
-          />
+          <button
+            onClick={() => setShowVideo(true)}
+            className="absolute flex items-center justify-center flex-col gap-4 "
+          >
+            <Image
+              className="button-image"
+              src="/playbutton.svg"
+              alt="Button Icon"
+              width={buttonSize}
+              height={buttonSize}
+            />
+            <p className="text-sm sm:text-xl">
+              {translations("play-sub-text")}
+            </p>
+          </button>
         </div>
-      )}
+
+        <div className="md:absolute pb-4 flex flex-grow flex-col gap-2 justify-start lg:left-[140px] xl:left-[240px] left-[100px] bottom-[100px] lg:bottom-[150px] ">
+          <h1 className="text-3xl sm:text-5xl font-plus-jakarta-sans text-white font-normal drop-shadow-2xl">
+            {translations("name")}
+          </h1>
+          <p className="text-white max-w-[60%] text-base">
+            {translations("role")}
+          </p>
+        </div>
+        {showVideo && (
+          <div
+            onClick={() => setShowVideo(false)}
+            className="fixed  flex flex-col items-center justify-center inset-0 z-10 bg-black bg-opacity-80 w-[100vw] h-[100vh]"
+          >
+            <iframe
+              title="vimeo-player"
+              src="https://player.vimeo.com/video/749148790?h=5fb7c065f9"
+              width="100%"
+              height="56.25%"
+              allowFullScreen
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
