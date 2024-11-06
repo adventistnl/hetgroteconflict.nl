fa62eed2d5d83231b7f0dc3a5c3e843ca018c960
Maykel Felipe Nekel <maykelnekel@gmail.com>
2024-11-05 23:09:39 -0300

    feat: add publishing houses array

--- src/components/organisms/publish-houses-container.tsx
+++ src/components/organisms/publish-houses-container.tsx
@@ -5,102 +5,142 @@ import { LinkIcon } from "../atoms/link-icon";
 import { EmailIcon } from "../atoms/icons/email-icon";
 import { GlobeIcon } from "../atoms/icons/globe-icon";
 
+interface publishHouse {
+  name: string;
+  languages: Array<string>;
+  location: string;
+  website?: string;
+  email?: string;
+}
 interface iProps {
   closeHandle: VoidFunction;
 }
 export const PublishHousesContainer = ({ closeHandle }: iProps) => {
   const translations = useTranslations("publish-houses");
-  const options = useMemo(
+  const publishHouses: publishHouse[] = useMemo(
     () => [
       {
-        name: "Africa Herald Publishing House",
-        languages: [
-          "English",
-          "Arabic",
-          "Greek",
-          "English",
-          "Arabic",
-          "Greek",
-          "English",
-          "Arabic",
-          "Greek",
-          "English",
-          "Arabic",
-          "Greek",
-        ],
-        location: "Kenya",
-        website: "https://example.com",
-        email: "info@example.com",
+        name: "Albanian Seventh-day Adventist Publishing Service",
+        languages: ["Albanian"],
+        location: "Albania",
+        email: "office@adventist.al",
       },
       {
-        name: "AAA",
+        name: "Croatian Publishing House",
+        languages: ["Croatian"],
+        location: "Croatia",
+        website: "https://www.znaci-vremena.com/",
+      },
+      {
+        name: "Slovenian Publishing House",
+        languages: ["Slovenian"],
+        location: "Slovenia",
+        email: "zalozba@adventisti.si",
+      },
+      {
+        name: "Latvian Publishing House",
+        languages: ["Latvian"],
+        location: "Latvia",
+        email: "lds@mail.bkc.lv",
+      },
+      {
+        name: "Lithuanian Conference Publishing Coordinator",
+        languages: ["Latvian"],
+        location: "Lithuania",
+        email: "office@adventistai.lt",
+      },
+      {
+        name: "Stanborough Press Limited",
+        languages: ["English"],
+        location: "United Kingdom",
+        website: "https://www.stanboroughpress.org.uk/",
+      },
+      {
+        name: "Danish Publishing House",
+        languages: ["Danish"],
+        location: "Denmark",
+        website: "https://danskbogforlag.dk/",
+      },
+      {
+        name: "Finland Seventh-day Adventist Publishing House",
+        languages: ["Finnish"],
+        location: "Finland",
+        website: "http://www.kirjatoimi.fi/",
+      },
+      {
+        name: "Greek Mission Publishing Centre",
         languages: ["Greek"],
-        location: "Kenya",
-        website: "https://example.com",
-        email: "info@example.com",
+        location: "Greece",
+        email: "yvrakas@adventist.gr",
       },
       {
-        name: "BBB",
-        languages: ["English"],
-        location: "Kenya",
-        website: "https://example.com",
-        email: "info@example.com",
+        name: "Advent Publishing House",
+        languages: ["Hungarian"],
+        location: "Hungary",
+        website: "http://adventkiado.hu/",
       },
       {
-        name: "BBB",
-        languages: ["English"],
-        location: "Kenya",
-        website: "https://example.com",
-        email: "info@example.com",
+        name: "Iceland Publishing House",
+        languages: ["Icelandic"],
+        location: "Iceland",
+        email: "gavin@adventistar.is",
       },
       {
-        name: "BBB",
-        languages: ["English"],
-        location: "Kenya",
-        website: "https://example.com",
-        email: "info@example.com",
+        name: "Netherlands Publishing Service",
+        languages: ["Dutch"],
+        location: "Netherlands",
+        website: "https://www.adventist.nl/",
       },
       {
-        name: "BBB",
-        languages: ["English"],
-        location: "Kenya",
-        website: "https://example.com",
-        email: "info@example.com",
+        name: "Norwegian Publishing House",
+        languages: ["Norwegian"],
+        location: "Norway",
+        website: "https://www.norskbokforlag.no/",
       },
       {
-        name: "BBB",
-        languages: ["English"],
-        location: "Kenya",
-        website: "https://example.com",
-        email: "info@example.com",
+        name: "Polish Publishing House",
+        languages: ["Polish"],
+        location: "Poland",
+        website: "https://znakiczasu.pl/",
       },
       {
-        name: "BBB",
-        languages: ["English"],
-        location: "Kenya",
-        website: "https://example.com",
-        email: "info@example.com",
+        name: "Macedonian Publishing House",
+        languages: ["Macedonian"],
+        location: "Republic of Macedonia",
+        website: "https://www.adventisti.org.mk/",
       },
-      // Adicione mais opções conforme necessário
+      {
+        name: "Serbian Publishing House",
+        languages: ["Serbian"],
+        location: "Serbia",
+        website: "https://www.preporod.rs/",
+      },
+      {
+        name: "Adventist Media and Forvaltning AB",
+        languages: ["Swedish"],
+        location: "Sweden",
+        email: "rainer.refsback@adventist.se",
+      },
     ],
     []
   );
 
   const [searchTerm, setSearchTerm] = useState("");
-  const [filteredOptions, setFilteredOptions] = useState(options);
+  const [filteredPublishHouses, setFilteredPublishHouses] =
+    useState(publishHouses);
 
   useEffect(() => {
-    setFilteredOptions(
-      options.filter((option) =>
+    setFilteredPublishHouses(
+      publishHouses.filter((option) =>
         option.languages.some((language) =>
           language.toLowerCase().includes(searchTerm.toLowerCase())
         )
       )
     );
-  }, [options, searchTerm]);
+  }, [publishHouses, searchTerm]);
 
   return (
-    <div className="fixed flex flex-col gap-4 p-4 text-primary bg-white max-w-[500px] z-[100] h-screen top-0 right-0">
+    <div className="fixed flex flex-col gap-4 p-4 text-primary bg-white w-full max-w-[500px] z-[100] h-screen top-0 right-0">
       <h2 className="text-xl">Publishing House</h2>
       <Input
         type="text"
@@ -113,7 +153,7 @@ export const PublishHousesContainer = ({ closeHandle }: iProps) => {
         X
       </button>
       <ul className="flex flex-col gap-4 overflow-y-auto">
-        {filteredOptions.map((option, index) => (
+        {filteredPublishHouses.map((option, index) => (
           <li
             key={index}
             className="flex flex-col border-b border-gray-500 gap-2 pb-6"
@@ -126,16 +166,20 @@ export const PublishHousesContainer = ({ closeHandle }: iProps) => {
               {translations("location")}: {option.location}
             </p>
             <div className="flex space-x-4 text-gray">
-              <LinkIcon
-                Icon={EmailIcon}
-                href={`mailto:${option.email}`}
-                text={translations("email")}
-              />
-              <LinkIcon
-                Icon={GlobeIcon}
-                href=""
-                text={translations("website")}
-              />
+              {option.email && (
+                <LinkIcon
+                  Icon={EmailIcon}
+                  href={`mailto:${option.email}`}
+                  text={translations("email")}
+                />
+              )}
+              {option.website && (
+                <LinkIcon
+                  Icon={GlobeIcon}
+                  href={option.website}
+                  text={translations("website")}
+                />
+              )}
             </div>
           </li>
         ))}
