5db6c63d5e8276d82ace2df86ccb125d2686a148
Maykel Felipe Nekel <maykelnekel@gmail.com>
2024-11-07 09:47:56 -0300

    feat: updating .next folder

---
+++ .next/server/_rsc_messages_zh_json.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "_rsc_messages_zh_json";
+exports.ids = ["_rsc_messages_zh_json"];
+exports.modules = {
+
+/***/ "(rsc)/./messages/zh.json":
+/*!**************************!*\
+  !*** ./messages/zh.json ***!
+  \**************************/
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"home-page-container":{"title":"大争论","subtitle":"发现关于人类命运的最深刻问题的答案。下载或在家接收揭示善恶之战的书籍，以及如何为未来做好准备。","download-button":"下载","receive-at-home-button":"在家接收"},"participate-page-container":{"video-container":{"name":"泰德·N·C·威尔逊","role":"基督复临安息日会主席","play-sub-text":"立即参与"},"quote-container":{"quote":"主感动我写这本书，以便*不延迟地*在*世界各地*传播，因为它所包含的警告对于准备一个在主的日子站立的民族是必要的。","author":"怀爱伦","book":"手稿 24","year":"1891"},"how-to-participate-container":{"title":"如何参与","step-sub":"步骤","step-one":{"step-count":"01","explanation":"向当地教会委员会介绍项目","link-sub":"下载委员会提案"},"step-two":{"step-count":"02","explanation":"选择外展区域","link-sub":""},"step-three":{"step-count":"03","explanation":"订购库存","link-sub":"寻找出版社"},"step-four":{"step-count":"04","explanation":"分发","link-sub":""}}},"publish-houses":{"language":"语言","location":"位置","email":"电子邮件","website":"网站","title":"出版社","placeholder":"按语言查找"},"book-cover-container":{"downloads":"下载","languages":"语言"},"summary-container":{"most-downloaded-title":"下载最多","summary":"摘要","paragraph-1":"您认为世界在变好还是变坏？不出所料，今天绝大多数人认为世界在变坏。也许这种全球悲观情绪是由于文化中充斥着坏消息，或者我们本能地知道这本书核心呈现的震撼真相：我们的星球出了严重问题，而我们无力解决。","paragraph-2":"《大争论》不仅揭示了人类堕落的起源，还揭示了在疫情和危险、腐败和屠杀、谋杀和混乱的喧嚣下进行的巨大斗争。在这部惊人的作品中，您将发现邪恶有面孔，善良有冠军，罪恶有终结。如果您想为这个世界的终结和即将到来的荣耀世界做好准备，您必须阅读这本书。"},"distribution-package-container":{"summary":"分发包","paragraph-1":"您准备好帮助分享希望的信息了吗？在这次《大争论》的特别分发活动中，我们提供了一整套免费的宣传材料，旨在支持在您的社区中传播这一强大的信息。","paragraph-2":"立即下载完整包，成为这个改变生活的运动的一部分！发挥创意，吸引您的社区，并帮助有效传播《大争论》的信息。"},"book-language-container":{"language":"语言："},"book-available-formats-container":{"available":"可用：","audio":"音频","promotional":"宣传材料","reading":"阅读计划","study":"学习指南","no-options":"我们还没有该语言的下载选项。"},"download-menubar":{"download":"下载","promotional":"宣传材料","reading":"阅读计划","study":"学习指南"},"select-language":{"no-results":"未找到结果。"},"select-country":{"netherlands":"荷兰","belgium":"比利时","germany":"德国"},"receive-at-home-container":{"title":"在家接收","subtitle":"拥抱转变！在《大争论》中发现改变生活的真理。请求您的副本，并在家门口找到希望。","form-errors":{"country":"请选择您的国家","name":"请输入您的姓名","email":"请输入您的电子邮件","valid-email":"请输入有效的电子邮件","street":"请输入您的街道","number":"请输入您的号码和补充（如果有）","city":"请输入您的城市","postCode":"请输入您的邮政编码"},"form-placeholders":{"name":"姓名","street":"街道","number":"号码和补充","city":"城市","postCode":"邮政编码"},"success-message":"订单处理中！请检查您的电子邮件。","error-message":"处理您的订单时出错。请稍后再试","loading":"提交中...","button":"立即订购"},"navbar":{"contact":"联系我们","participate":"参与","home":"首页"},"talk-to-us-container":{"title":"联系我们","form-errors":{"name":"请输入您的姓名","email":"请输入您的电子邮件","valid-email":"请输入有效的电子邮件","message":"请输入您的消息"},"form-placeholders":{"name":"姓名","message":"消息"},"success-message":"消息发送成功！","error-message":"发送您的消息时出错。请稍后再试","loading":"发送中...","button":"发送"},"footer":{"policy":"隐私政策","legal":"法律声明","tradmark":"商标和标志使用"}}');
+
+/***/ })
+
+};
+;
